f3271278fbda61a721dc27cba505532faa67040c
TalLancaster <tal.lancaster+gh@gmail.com>
2019-10-03 13:02:03 -0700

    core/admin.py: Added Document support
    So can interact via admin interface.

--- uploads/core/admin.py
+++ uploads/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Document
+
+class DocumentAdmin(admin.ModelAdmin):
+    exclude = []
+
+admin.site.register(Document, DocumentAdmin)
